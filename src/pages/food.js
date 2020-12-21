5322cf51cf1989547bd2694867508b94779fddbd
Lukie Kang <lukiek@gmail.com>
2020-12-21 00:41:02 +0000

    food graphql query

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,11 +1,36 @@
 import React from 'react';
 
-function FoodPage() {
+function FoodPage({ data }) {
+  const dishes = data.dishes.edges;
   return (
     <div>
-      <p>This is the Food page</p>
+      <h1>We currently serve {dishes.length} dishes</h1>
     </div>
   );
 }
+
+export const DishQuery = graphql`
+  query DishQuery {
+    dishes: allSanityDish {
+      edges {
+        node {
+          id
+          name
+          price
+          Intolerences {
+            name
+          }
+          image {
+            asset {
+              fluid(maxWidth: 400) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default FoodPage;
